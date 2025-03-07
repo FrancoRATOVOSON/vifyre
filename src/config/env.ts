ea0b328a11c6f11273312450b3870fc96935e5f9
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2025-03-07 23:53:54 +0300

    add config

---
+++ src/config/env.ts
@@ -0,0 +1,25 @@
+import { cleanEnv, makeValidator, str } from 'envalid'
+
+const appPortValidator = makeValidator<number>(value => {
+  const port = Number.parseInt(value, 10)
+  if (Number.isNaN(port)) throw new Error('Error when getting port from .env')
+  return port
+})
+
+export const env = cleanEnv(
+  process.env,
+  {
+    PORT: appPortValidator(),
+    NODE_ENV: str({
+      choices: ['development', 'test', 'production'],
+      default: 'development'
+    })
+  },
+  {
+    reporter: ({ errors }) => {
+      for (const [envVariable, error] of Object.entries(errors)) {
+        console.error(`Environment variable error - on ${envVariable} [${error}]`)
+      }
+    }
+  }
+)
