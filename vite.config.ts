bb83100b580905fa2d9e3e0889ba31999b4022b3
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2025-03-09 19:35:46 +0300

    configure vite & deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(() => ({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()]
+  plugins: [
+    tailwindcss(),
+    react(),
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    tsconfigPaths()
+  ]
 }))
